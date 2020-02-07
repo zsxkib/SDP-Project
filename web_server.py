d0765575cdd3e2d4a392cf4bbc63e85377d6d8f9
Shivamm <s1735229@sms.ed.ac.uk>
2020-02-07 19:09:04 +0000

    made a function/method to convert the classification output into the correct bin based on the number of bins the system is using

--- web_server.py
+++ web_server.py
@@ -4,9 +4,40 @@ import numpy as np
 import pickle
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage:', sys.argv[0], 'classifier_pickle')
+if len(sys.argv) != 3:
+    print('Usage:', sys.argv[0], 'classifier_pickle', 'number_of_bins')
     sys.exit(1)
+
+##datastructure to help support classification of images based on the number of
+##bins that are in the system
+common_map = {
+'recyclable': 'recyclable',
+'non-recyclable': 'non-recyclable',
+'glass': 'recyclable',
+'plastic': 'recyclable',
+'paper': 'recyclable',
+'trash': 'non-recyclable',
+}
+MAP = {
+2:{
+    'metal': 'recyclable',
+    'cardboard': 'recyclable',
+    **common_map
+},
+3:{
+    'metal': 'metal',
+    'cardboard': 'recyclable',
+    **common_map
+},
+4:{
+    'metal': 'metal',
+    'cardboard': 'cardboard',
+    **common_map
+}
+}[sys.argv[2]]
+
+
+
 
 app = Flask(__name__)
 cfer = pickle.load(open(sys.argv[1], 'rb'))
@@ -14,7 +45,7 @@ print('loaded', cfer)
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    return cfer.classify( dict(np.load(BytesIO(request.data))) )
+    return MAP[ cfer.classify( dict(np.load(BytesIO(request.data))) ) ]
 
 @app.route('/update/<label>', methods=['GET'])
 def update(label):
