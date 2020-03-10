be6c42ad429d056c557a2c1b8c284eb1ac51d133
Anshul Agarwal <s1776014@ed.ac.uk>
2020-03-10 17:21:11 +0000

    working pi-arduino connection

--- electroMagnet.py
+++ electroMagnet.py
@@ -5,7 +5,11 @@ em = Motors()
 magnet1 = 4
 magnet2 = 5
 def runMagnet():
+<<<<<<< Updated upstream
 #	em.move_motor(magnet1, 100)
+=======
+	#em.move_motor(magnet1, 100)
+>>>>>>> Stashed changes
 	em.move_motor(magnet2, 100)
 def stopMagnet():
 	em.stop_motors()
