70dfb63218a6c512b8911265bad9629798369eb4
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-10 17:26:23 +0000

    recieve and send data on arduino

--- arduino/serial connection/serial_test/serial_test.ino
+++ arduino/serial connection/serial_test/serial_test.ino
@@ -1,22 +1,10 @@
-//char dataString[50] = {0};
-int a =0; 
-
 void setup() {
-Serial.begin(9600); //Starting serial communication
-Serial.println(100);
+  Serial.begin(9600);
 }
-
 void loop() {
-  a++;                          // a value increase every loop
-  //sprintf(dataString,"%02X",a); // convert a value to hexa 
-  Serial.println(a);   // send the data 
-  delay(1000);                  // give the loop some break
-  Serial.println("shaktiman");
-  loop2();
-}
-
-void loop2(){
-  int s = Serial.read();
-  Serial.println(s);
-  delay(1000);
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.print("You sent me: ");
+    Serial.println(data);
+  }
 }
