a360696f7111b7769431bfba04458e52321ac5f5
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-10 18:05:54 +0000

    move.py_arduino

--- arduino/Move/move.py
+++ arduino/Move/move.py
@@ -3,9 +3,9 @@ from time import sleep
 
 if (__name__=='__main__'):
 	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
-    
-    ser.write(b"move_front\n")
+	ser.flush()
+	
+	ser.write(b"move_front\n")
 	time.sleep(2)
 	ser.write(b"stop_motors\n")
 	time.sleep(2)
