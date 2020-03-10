22a63d98571c09c1d4bae00ddf7595b44f7524cd
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-10 17:29:47 +0000

    recieve and send data on arduino

--- arduino/serial connection/seria_test.py
+++ arduino/serial connection/seria_test.py
@@ -1,8 +1,10 @@
 import serial
-
-ser = serial.Serial('/dev/ttyACM0',9600)
-s = [0,1]
-while True:
-	read_serial=ser.readline()
-	print (read_serial)
-	ser.print(1024)
+import time
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
