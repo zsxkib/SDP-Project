ed2c8d60f847a59bd376cce7c4c5c9ccb9514723
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-12 17:48:24 +0000

    Trial 2.4

--- arduino/Move/move.py
+++ arduino/Move/move.py
@@ -1,31 +1,42 @@
 import serial
 from time import sleep
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
 
 def read():
 	line = ser.readline().decode('utf-8').rstrip()
 	print(line)
 
 if (__name__=='__main__'):
-	
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 	ser.flush()
 	ser.write(b"hi\n")
 	sleep(2)
-	read()
-
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
 	ser.write(b"move_front\n")
 	sleep(2)
-	read()
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
 	ser.write(b"stop_motors\n")
 	sleep(2)
-	read()
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
 	ser.write(b"move_back\n")
 	sleep(2)
-	read()
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
 	ser.write(b"hatch\n")
 	sleep(2)
-	read()
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
 	ser.write(b"stop_motors")
 	sleep(2)
-	read()
+	#read()
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
