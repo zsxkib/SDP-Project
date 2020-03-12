8bab42833ce0ee67be8f3a447e7d993ab1a77f6c
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-12 17:22:53 +0000

    Trial 2.2

--- arduino/serial connection/seria_test.py
+++ arduino/serial connection/seria_test.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 	ser.flush()
 	i=0
 	while True:
-		ser.write(b"Hello from Raspberry Pi! "+str(i)+"\n")
+		ser.write(b("Hello from Raspberry Pi! "+str(i)+"\n"))
 		i+=1
 		line = ser.readline().decode('utf-8').rstrip()
 		print(line)
