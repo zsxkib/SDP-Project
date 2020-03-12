3b92e255c59794834837ea6b1a2e02cdb41f78a1
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-12 17:43:02 +0000

    Trial 2.3

--- arduino/Move/Move.ino
+++ arduino/Move/Move.ino
@@ -3,47 +3,50 @@
 
 void setup() {
   Serial.begin(9600);
+  SDPsetup();
+  helloWorld();
 
 }
 
 void loop() {
   if (Serial.available()>0){
     String data = Serial.readStringUntil('\n');
-//    if (data.equals("move_front")){
-//      Serial.print("You sent me: hello");
-//      move_front();
-//    }
-//    else if (data.equals("move_back")){
-//      move_back();
-//    }
-//    else if (data.equals("hatch")){
-//      hatch();
-//    }
-//    else if (data.equals("stop_motors")){
-//      stop_motors();
-//    }
-//    else {
-//      stop_all();
-//    } 
-//    move_front();
-//    delay(500);
-//    stop_all();
+    if (data.equals("move_front")){
+      move_front();
+    }
+    else if (data.equals("move_back")){
+      move_back();
+    }
+    else if (data.equals("hatch")){
+      hatch();
+    }
+    else if (data.equals("stop_motors")){
+      stop_motors();
+    }
+    else {
+      stop_all();
+    } 
     Serial.print("You sent me: ");
     Serial.println(data);
   }
 }
 
 void move_front(){
-  motorForward(5,100);
-  motorForward(4,100);
-  motorForward(3,100);
-  motorForward(2,100);
-  motorForward(1,100);
-  motorForward(0,100);
+  motorForward(0, 100);
+  motorForward(1, 100);
+  motorForward(2, 100);
+  motorForward(3, 100);
+  motorForward(4, 100);
+  motorForward(5, 100);
 }
 
 void move_back(){
-  motorBackward(5,100);
+  motorBackward(0, 100);
+  motorBackward(1, 100);
+  motorBackward(2, 100);
+  motorBackward(3, 100);
+  motorBackward(4, 100);
+  motorBackward(5, 100);
 }
 
 void hatch(){
