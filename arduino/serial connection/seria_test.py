f91468daf231f89f7421970f9ed5f6d93692821d
Anshul Agarwal <anshul28101997@gmail.com>
2020-03-12 17:21:54 +0000

    Trial 2.2

--- arduino/serial connection/seria_test.py
+++ arduino/serial connection/seria_test.py
@@ -1,12 +1,12 @@
 import serial
 import time
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+	ser.flush()
 	i=0
-    while True:
-        ser.write(b"Hello from Raspberry Pi! "+str(i)+"\n")
+	while True:
+		ser.write(b"Hello from Raspberry Pi! "+str(i)+"\n")
 		i+=1
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+		line = ser.readline().decode('utf-8').rstrip()
+		print(line)
+		time.sleep(1)
